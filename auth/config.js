8b1681903006c17945861e7551dcdfa9e56a4fb9
Jaden <hyunbin.kim.3376@gmail.com>
2025-06-19 20:03:20 +0900

    feat: 깃허브 로그인 구현 완료

---
+++ auth/config.js
@@ -0,0 +1,6 @@
+export const GITHUB_CONFIG = {
+  LOGIN_URL: 'https://github.com/login/oauth/authorize',
+  LOGOUT_URL: 'https://github.com/logout',
+  CLIENT_KEY: 'Ov23ctbbq5J8bocxUM3E',
+  OAUTH_STATE: 'oauth_state',
+};
