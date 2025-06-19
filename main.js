2e8c41693c53c32334c20c47bced13ac6d61f5d4
kimym98 <tampler48@gmail.com>
2025-06-19 22:45:15 +0900

    rank data 변경

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ import { setBoardDataList, setRankData } from "./lib/dom/data.js";
 import { bindingEvent } from "./lib/event/listener.js";
 document.addEventListener("DOMContentLoaded", init);
 
-
-function init(){
+function init() {
   bindingEvent();
   console.log("init 실행됨");
   setBoardDataList("freeBoard");
-  setRankData("rank")
+  setRankData("team");
+  setRankData("personal");
 }
